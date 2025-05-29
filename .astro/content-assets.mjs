8669176c56a79a33d528378c2d0e446708111d25
RGLabX <rgillis@labxchange.io>
2025-05-29 14:19:00 -0400

    initial check in part 7

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_HnWsA from "/src/assets/LXStool.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["/src/assets/LXStool.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_HnWsA]]);
+		
